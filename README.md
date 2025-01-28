e1be60c4d3e9fb942f8e0a72554ae543401bde3e
Eduardo Konzen <eduardokonz@gmail.com>
2025-01-28 01:23:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do site TabNews, referente ao Curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Rafaela, sabia que seu sorriso tem o poder de transformar qualquer dia comum no mais especial de todos?</h1>
+  return (
+    <h1>
+      Rafaela, sabia que seu sorriso tem o poder de transformar qualquer dia
+      comum no mais especial de todos?
+    </h1>
+  );
 }
 
 export default Home;
